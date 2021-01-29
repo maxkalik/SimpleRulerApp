7eff5b552dd2508166a05c08819cad34523a7fe2
Maksim Kalik <maxkalik@gmail.com>
2021-01-29 15:27:42 +0200

    moving not specific cone

--- SimpleRulerApp/Controllers/ViewController.m
+++ SimpleRulerApp/Controllers/ViewController.m
@@ -11,6 +11,8 @@
 
 @property (nonatomic, strong) IBOutlet ARSCNView *sceneView;
 @property (nonatomic, strong) NSMutableArray<SCNNode*> *markerNodes;
+
+@property (nonatomic, assign) CGFloat panStartZ;
 @end
 
     
@@ -23,6 +25,38 @@
     self.sceneView.autoenablesDefaultLighting = YES;
     
     self.markerNodes = [[NSMutableArray alloc] init];
+    
+    UIGestureRecognizer* panRecongnizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(dragObject:)];
+    [self.sceneView addGestureRecognizer:panRecongnizer];
+}
+
+- (void)dragObject:(UIPanGestureRecognizer*)sender {
+    // NSLog(@"%f", [sender locationInView:self.sceneView].x);
+    CGPoint touchLocation = [sender locationInView:self.sceneView];
+    ARHitTestResult* hitTestResut = [self.sceneView hitTest:touchLocation types:ARHitTestResultTypeFeaturePoint].firstObject;
+    if (self.markerNodes.count > 0) {
+        simd_float4 location = hitTestResut.worldTransform.columns[3];
+        [self.markerNodes objectAtIndex:0].position = SCNVector3Make(location.x, location.y, location.z);
+    }
+    
+    
+    // switch (sender.state) {
+    //     case UIGestureRecognizerStateBegan: {
+    //         ARHitTestResult* hitTestResut = [self.sceneView hitTest:touchLocation types:ARHitTestResultTypeFeaturePoint].lastObject;
+    //         // NSLog(@"began %f", hitTestResut.worldTransform.columns[3].z);
+    //         // NSLog(@"began last pan location %@", hitTestResut.worldTransform);
+    //         self.panStartZ = hitTestResut.worldTransform.columns[3].z;
+    //     }
+    //     case UIGestureRecognizerStateChanged: {
+    //         if (self.markerNodes.count > 0) {
+    //             SCNVector3 worldTouchPosition = [self.sceneView unprojectPoint:SCNVector3Make(touchLocation.x, touchLocation.y, self.panStartZ)];
+    //             markerNode.position = SCNVector3Make(location.x, location.y, location.z);
+    //
+    //             NSLog(@"changed %f", touchLocation.x);
+    //         }
+    //     }
+    //     default: break;
+    // }
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -41,9 +75,7 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     CGPoint touchLocation = [touches.allObjects.firstObject locationInView:self.sceneView];
     ARHitTestResult* hitTestResut = [self.sceneView hitTest:touchLocation types:ARHitTestResultTypeFeaturePoint].firstObject;
-    if (hitTestResut == nil) {
-        return;
-    }
+    if (hitTestResut == nil) { return; }
     
     // NSLog(@"%@", hitTestResut);
     [self addMarkerAt:hitTestResut];
